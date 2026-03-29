99e872e6711d447a1acadf2f365bb5198f772a21
Cindy4869 <2948292626@qq.com>
2026-03-29 03:57:07 -0500

    trying to deploy

--- config/secrets.js
+++ config/secrets.js
@@ -1,5 +1,5 @@
 module.exports = {
   token: "secret-starter-mern",
   mongo_connection:
-    "mongodb+srv://ruoanw2:photoverse@cluster0.louf2.mongodb.net/",
+    "mongodb+srv://xinyiye4:PhotoVerse2026@cluster0.k5mcx.mongodb.net/?appName=Cluster0",
 };
